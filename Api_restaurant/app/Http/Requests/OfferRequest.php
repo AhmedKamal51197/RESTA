0c9eb08dc2ae6cca96878897d67f9e2c6640f2c1
Ahmed Kamal mahmoud <eng.ahmedkamal357@gmail.com>
2024-07-30 01:09:20 +0300

    deleted offer & items & get all Items for front

---
+++ Api_restaurant/app/Http/Requests/OfferRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OfferRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=>['required','string','min:6','unique:offers,name'],
+            'discount'=>['required','integer'],
+            'startDate'=>['required','date','before:endDate'],
+            'endDate'=>['required','date','after:startDate'],
+            'image'=>['image','mimes:jpeg,png,jpg,gif,svg','max:2048'],
+            'status'=>['required','in:active,inactive']
+
+        ];
+    }
+}
